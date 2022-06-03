1320a12d4544cd83c16744841ae43bb8408e20c9
Aashim Bajracharya <ashimbazracharya@gmail.com>
2022-06-03 12:12:42 +0545

    [ADD] Add module to enter subtotal amount in invoiceline and get respected unit price

---
+++ account_move_editable_total_amount/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Account Move Editable Subtotal Amount",
+
+    'summary': """
+        Provision to edit subtotal amount and get the unit price.
+    """,
+
+    'author': "Ten Orbits Pvt. Ltd.",
+    'website': "10orbits.com",
+
+    'category': 'Account',
+    'version': '14.0.0.0.1',
+    'sequence': 1,
+    'application': True,
+    'depends': ['account'],
+
+    'data': [
+        'views/account_move.xml'
+    ],
+}
---
+++ account_move_editable_total_amount/models/account_invoice.py
@@ -0,0 +1,18 @@
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.invoice.line'
+
+    editable_price_total = fields.Float(string="Edited Amount", default=0.0)
+
+
+    @api.onchange('editable_price_total', 'quantity')
+    def onchange_editable_pricetotal(self):
+        if self.editable_price_total and self.quantity:
+            
+            # Remove the selected taxes
+            self.invoice_line_tax_ids = None
+
+            # Get the unit price and assign to the related field
+            self.price_unit = round(self.editable_price_total/self.quantity, 3)
